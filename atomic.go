74da1703af2abc1e07bb7c892ac78a67d80195f6
Bryan Konowitz <bryan@kono.sh>
2021-08-13 21:21:31 -0700

    refactor: swapped from sync.Cond to channel for context support

--- atomic.go
+++ atomic.go
@@ -1,6 +1,8 @@
 package concurrency
 
 import (
+	"context"
+	"log"
 	"sync"
 	"sync/atomic"
 )
@@ -8,21 +10,40 @@ import (
 // AtomicBool supports safe concurrency when setting, checking, and waiting for a boolean value.
 type AtomicBool struct {
 	value int32
-	c     *sync.Cond
-	m     *sync.Mutex
+
+	wait chan struct{}
+	waitLock *sync.RWMutex
 }
 
 // Set sets the boolean to given value.
 func (ab *AtomicBool) Set(ok bool) {
-	var val int32
 	if ok {
-		val = 1
+		ab.setTrue()
 	} else {
-		val = 0
+		ab.setFalse()
+	}
+}
+
+func (ab *AtomicBool) setTrue() {
+	if ab.Value() {
+		return
 	}
 
+	ab.waitLock.Lock()
+	defer ab.waitLock.Unlock()
+	var val int32 = 1
 	atomic.StoreInt32(&ab.value, val)
-	ab.c.Broadcast()
+	close(ab.wait)
+	ab.wait = make(chan struct{})
+}
+
+func (ab *AtomicBool) setFalse() {
+	if !ab.Value() {
+		return
+	}
+
+	var val int32 = 0
+	atomic.StoreInt32(&ab.value, val)
 }
 
 // Value returns the current status.
@@ -30,23 +51,31 @@ func (ab *AtomicBool) Value() bool {
 	return atomic.LoadInt32(&ab.value)&1 == 1
 }
 
-// WaitForTrue causes the caller to block until the AtomicBool is set to true.
-func (ab *AtomicBool) WaitForTrue() bool {
-	ab.c.L.Lock()
-	for !ab.Value() {
-		ab.c.Wait()
+func (ab *AtomicBool) WaitWithContext(ctx context.Context) bool {
+	ab.waitLock.RLock()
+	ch := ab.wait
+	ab.waitLock.RUnlock()
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("ctx done")
+			return ab.Value()
+		case <-ch:
+			log.Println("received from wait chan")
+			return ab.Value()
+		}
 	}
-	ab.c.L.Unlock()
+}
 
-	return true
+// Wait causes the caller to block until the AtomicBool is set to true.
+func (ab *AtomicBool) Wait() bool {
+	return ab.WaitWithContext(context.Background())
 }
 
 // NewAtomicBool returns a ready for use AtomicBool.
 func NewAtomicBool() *AtomicBool {
-	m := &sync.Mutex{}
-	c := sync.NewCond(m)
 	return &AtomicBool{
-		m: m,
-		c: c,
+		waitLock: &sync.RWMutex{},
+		wait: make(chan struct{}),
 	}
 }
--- atomic_test.go
+++ atomic_test.go
@@ -1,9 +1,11 @@
 package concurrency
 
 import (
+	"context"
 	"fmt"
 	"sync"
 	"testing"
+	"time"
 )
 
 func TestAtomicBool_Set(t *testing.T) {
@@ -19,29 +21,104 @@ func TestAtomicBool_Set(t *testing.T) {
 	}
 }
 
-func TestAtomicBool_WaitForTrue(t *testing.T) {
+func TestAtomicBool_Wait(t *testing.T) {
 	ab := NewAtomicBool()
 	var readyGroup sync.WaitGroup
 	var doneGroup sync.WaitGroup
 
-	worker := func(count int) {
+	worker := func(count int, results []bool) {
 		readyGroup.Add(count)
 		doneGroup.Add(count)
 		for i := 1; i <= count; i++ {
 			go func(num int) {
 				readyGroup.Done()
 				fmt.Printf("go routine %d, launched\n", num)
-				ab.WaitForTrue()
+				val := ab.Wait()
+				results[num-1] = val
 				fmt.Printf("go routine %d received true\n", num)
 				doneGroup.Done()
 			}(i)
 		}
 	}
-	worker(2)
+	count := 2
+	results := make([]bool, count)
+	worker(2, results)
 	readyGroup.Wait()
 
 	fmt.Println("setting true")
 	ab.Set(true)
 	fmt.Println("set")
 	doneGroup.Wait()
+
+	for _, r := range results {
+		if !r {
+			t.Errorf("expected all results to be true, found false")
+		}
+	}
+}
+
+func TestAtomicBool_WaitWithContext(t *testing.T) {
+
+	var readyGroup sync.WaitGroup
+	var doneGroup sync.WaitGroup
+
+	worker := func(count int, ab *AtomicBool, ctx context.Context, results []bool) {
+		readyGroup.Add(count)
+		doneGroup.Add(count)
+		for i := 1; i <= count; i++ {
+			go func(num int) {
+				readyGroup.Done()
+				fmt.Printf("go routine %d, launched\n", num)
+				val := ab.WaitWithContext(ctx)
+				results[num-1] = val
+				fmt.Printf("go routine %d received %v\n", num, val)
+				doneGroup.Done()
+			}(i)
+		}
+	}
+
+	t.Run("clears wait when set to true", func(t *testing.T) {
+		ab := NewAtomicBool()
+
+		ab.Set(false)
+		count := 2
+		results := make([]bool, count)
+		ctx, _ := context.WithTimeout(context.Background(), 3*time.Second)
+		worker(2, ab, ctx, results)
+		readyGroup.Wait()
+
+		fmt.Println("setting true")
+		ab.Set(true)
+		fmt.Println("set")
+		doneGroup.Wait()
+
+		for _, r := range results {
+			if !r {
+				t.Errorf("expected all results to be true, found false")
+			}
+		}
+	})
+
+	t.Run("clears early when context is done", func(t *testing.T) {
+		ab := NewAtomicBool()
+		count := 2
+		results := make([]bool, count)
+		ctx, cancel := context.WithCancel(context.Background())
+
+		ab.Set(false)
+		worker(2, ab, ctx, results)
+		readyGroup.Wait()
+
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println("cancelling context...")
+		cancel()
+		fmt.Println("... cancelled")
+		doneGroup.Wait()
+
+		for _, r := range results {
+			if r {
+				t.Errorf("expected all results to be false due to cancellation, found true")
+			}
+		}
+	})
 }
